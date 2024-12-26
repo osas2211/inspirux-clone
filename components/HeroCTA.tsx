05322a89756d6901bde0cba1a1056e7667436650
Osaretin Frank <osaretin.frank10@gmail.com>
2024-12-26 10:44:53 +0100

    add video to hero

--- components/HeroCTA.tsx
+++ components/HeroCTA.tsx
@@ -1,17 +1,52 @@
 "use client"
 import { PlayIcon } from "@/icons/PlayIcon"
 import { ScrollDownIcon } from "@/icons/ScrollDownIcon"
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 
 export const HeroCTA = () => {
+  const [showVideo, setShowVideo] = useState(false)
+  const playRef: React.Ref<HTMLDivElement> = useRef(null)
+  const toggleVideo = () => setShowVideo(!showVideo)
+  useEffect(() => {
+    playRef.current?.addEventListener("mouseenter", () => {
+      toggleVideo()
+    })
+    playRef.current?.addEventListener("mouseleave", () => {
+      toggleVideo()
+    })
+
+    // return ()=> {
+    //   playRef.current?.removeEventListener("mouseleave", ()=> {})
+    // }
+  }, [showVideo])
   return (
     <>
       <div className="md:h-[14vh] h-auto w-full grid md:grid-cols-[50%,9%,41%] grid-cols-[1fr,15%,60%] uppercase text-xs relative">
         <div className="flex justify-between items-center w-full md:h-full h-[95px] bg-secondary text-white px-5 md:px-10 md:col-span-1 col-span-3">
           <p className="">Explore works</p>
         </div>
-        <div className="flex justify-center items-center w-full md:h-full h-[95px] bg-primary text-white px-5 md:px-10">
-          <PlayIcon />
+        <div
+          className={`${
+            !showVideo ? "px-5 md:px-10" : ""
+          } flex justify-center items-center w-full md:h-full h-[95px] bg-primary text-white relative cursor-pointer`}
+          ref={playRef}
+        >
+          {showVideo ? (
+            <video className="w-full h-full object-cover" autoPlay loop muted>
+              <source
+                src="/assets/videos/selected-works-vid.mp4"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+          ) : (
+            <>
+              <PlayIcon />
+            </>
+          )}
+          {/* <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            
+          </div> */}
         </div>
         <div className="flex justify-between items-center w-full md:h-full h-[95px] bg-[#eee] px-5 md:px-10 md:col-span-1 col-span-2">
           <p className="">Play reel</p>
